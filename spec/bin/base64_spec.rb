76427997ae3403cb4d8312975c37e0ed0846166b
Marco Imperatore <mimperatore@gmail.com>
2023-06-15 19:47:06 -0400

    add arg checks

--- spec/bin/base64_spec.rb
+++ spec/bin/base64_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe 'base64' do
   it 'produces the correct inverse' do
     expect(pexec('bin/base64', 'test content'))
       .to be_operator('base64 -d')
+      .with_no_args
       .with_content('dGVzdCBjb250ZW50')
   end
 
   it 'produces the correct inverse also when the content contains a trailing newline' do
     expect(pexec('bin/base64', "test content\n"))
       .to be_operator('base64 -d')
+      .with_no_args
       .with_content('dGVzdCBjb250ZW50Cg==')
   end
 end
@@ -20,12 +22,14 @@ RSpec.describe 'base64 -d' do
   it 'produces the correct inverse' do
     expect(pexec('bin/base64 -d', 'dGVzdCBjb250ZW50'))
       .to be_operator('base64')
+      .with_no_args
       .with_content('test content')
   end
 
   it 'produces the correct inverse also when the decoded content contains a trailing newline' do
     expect(pexec('bin/base64 -d', 'dGVzdCBjb250ZW50Cg=='))
       .to be_operator('base64')
+      .with_no_args
       .with_content("test content\n")
   end
 end
--- spec/bin/cat_spec.rb
+++ spec/bin/cat_spec.rb
@@ -6,12 +6,14 @@ RSpec.describe 'cat' do
   it 'produces the correct inverse' do
     expect(pexec('bin/cat', 'test content'))
       .to be_operator('cat')
+      .with_no_args
       .with_content('test content')
   end
 
   it 'produces the correct inverse also when the content contains a trailing newline' do
     expect(pexec('bin/cat', "test content\n"))
       .to be_operator('cat')
+      .with_no_args
       .with_content("test content\n")
   end
 end
