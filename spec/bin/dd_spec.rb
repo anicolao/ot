ce181ee5aeaccc9cfe20167438dc6686db9322a1
Marco Imperatore <mimperatore@gmail.com>
2023-06-15 19:54:44 -0400

    extract directory where files are to be tored to variable

--- spec/bin/dd_spec.rb
+++ spec/bin/dd_spec.rb
@@ -4,12 +4,13 @@ require 'spec_helper'
 
 RSpec.describe 'dd status=none' do
   let(:content) { 'test content' }
+  let(:filestore_dir) { "#{ENV['HOME'].chomp('/')}/.ot/" }
 
   it 'produces the correct inverse' do
     sha256sum = pexec('sha256sum -bz', content).split(' ')[0]
     expect(pexec("bin/dd status=none", content))
       .to be_operator("dd status=none if=%{dir}%{file}")
-      .with_args(dir: "#{ENV['HOME'].chomp('/')}/.ot/", file: sha256sum)
+      .with_args(dir: filestore_dir, file: sha256sum)
       .with_content('')
   end
 end
