ba5901bf0bf0d8876d788b45635b89d3a67ad939
Marco Imperatore <mimperatore@gmail.com>
2023-06-15 20:07:51 -0400

    fix test

--- spec/bin/dd_spec.rb
+++ spec/bin/dd_spec.rb
@@ -17,11 +17,13 @@ end
 
 RSpec.describe 'dd status=none if=%{dir}%{file}' do
   let(:content) { 'test content' }
+  let(:filestore_dir) { "#{ENV['HOME'].chomp('/')}/.ot/" }
 
   it 'produces the correct inverse' do
     sha256sum = pexec('sha256sum -bz', content).split(' ')[0]
-    expect(pexec("bin/dd status=none if=%{dir}%{file}", sha256sum))
+    expect(pexec("bin/dd status=none if=#{filestore_dir}#{sha256sum}", ''))
       .to be_operator("dd status=none")
-      .with_content(sha256sum)
+      .with_no_args
+      .with_content(content)
   end
 end
