904dfd896858a61b8f01d973e3e24602088b6618
Marco Imperatore <mimperatore@gmail.com>
2023-06-17 13:11:39 -0400

    add test coverage to Cmd::Generic

--- spec/cmds/generic_spec.rb
+++ spec/cmds/generic_spec.rb
@@ -4,21 +4,26 @@ require 'spec_helper'
 
 RSpec.describe Cmds::Generic do
   describe '.exec' do
+    let(:fwd_op) { spy('fwd op') }
+    let(:inv_op) { spy('inv op') }
+    let(:binmode_stdout) { spy('binmode stdout') }
+
     let(:fwd_args) { {a: 1, b: 2} }
     let(:inv_args) { {x: 7, y: 8} }
 
     let(:input_stream) { StringIO.new('input stream') }
-    let(:fwd_output) { 'forward output' }
+    let(:fwd_out) { 'forward output' }
     let(:serialized_inv_op) { 'serialized inv op' }
 
+    before :each do
+      allow($stdout).to receive(:binmode).and_return(binmode_stdout)
+      allow(binmode_stdout).to receive(:write)
+    end
+
     it 'outputs the serialized version of the inverse operator to $stdout in binmode' do
-      fwd_op = spy('fwd op')
-      inv_op = spy('inv op')
+      expect(fwd_op).to receive(:exec).with(args: fwd_args, input_stream: input_stream).and_return(fwd_out)
+      expect(inv_op).to receive(:serialize).with(args: inv_args, content: fwd_out).and_return(serialized_inv_op)
 
-      expect(fwd_op).to receive(:exec).with(args: fwd_args, input_stream: input_stream).and_return(fwd_output)
-      expect(inv_op).to receive(:serialize).with(args: inv_args, content: fwd_output).and_return(serialized_inv_op)
-
-      binmode_stdout = spy('binmode stdout')
       expect($stdout).to receive(:binmode).and_return(binmode_stdout)
       expect(binmode_stdout).to receive(:write).with(serialized_inv_op)
 
@@ -27,6 +32,40 @@ RSpec.describe Cmds::Generic do
         input_stream: input_stream,
         inv_op: inv_op, inv_args: inv_args
       )
+    end
+
+    context 'when a block is given to it' do
+      let(:revised_output) { 'revised output' }
+      let(:revised_inv_args) { {x: 77, y: 88} }
+
+      it "is passed the fwd_op's output and inv_args" do
+        allow(fwd_op).to receive(:exec).with(args: fwd_args, input_stream: input_stream).and_return(fwd_out)
+
+        described_class.exec(
+          fwd_op: fwd_op, fwd_args: fwd_args,
+          input_stream: input_stream,
+          inv_op: inv_op, inv_args: inv_args
+        ) do |fwd_output:, inv_args:|
+          expect(fwd_output).to eq(fwd_out)
+          expect(inv_args).to eq(inv_args)
+          {}
+        end
+      end
+
+      it 'uses the returned fwd_output and inv_args when serializing the inverse operation' do
+        expect(inv_op).to receive(:serialize).with(args: revised_inv_args, content: revised_output)
+
+        described_class.exec(
+          fwd_op: fwd_op, fwd_args: fwd_args,
+          input_stream: input_stream,
+          inv_op: inv_op, inv_args: inv_args
+        ) do |fwd_output:, inv_args:|
+          {
+            fwd_output: revised_output,
+            inv_args: revised_inv_args
+          }
+        end
+      end
     end
   end
 end
