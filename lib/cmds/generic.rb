5920d31b5361816a9b85977ffbc009c00ac9e80b
Marco Imperatore <mimperatore@gmail.com>
2023-06-17 14:38:08 -0400

    use self. instead of class << self

--- lib/cmds/generic.rb
+++ lib/cmds/generic.rb
@@ -2,23 +2,21 @@
 
 module Cmds
   class Generic
-    class << self
-      def exec(
-        fwd_op:, fwd_args:, input_stream:,
-        inv_op:, inv_args:
-      )
-        fwd_output = fwd_op.exec(args: fwd_args, input_stream: input_stream)
+    def self.exec(
+      fwd_op:, fwd_args:, input_stream:,
+      inv_op:, inv_args:
+    )
+      fwd_output = fwd_op.exec(args: fwd_args, input_stream: input_stream)
 
-        if block_given?
-          return_hash = yield(fwd_output: fwd_output, inv_args: inv_args)
-          fwd_output = return_hash[:fwd_output] if return_hash[:fwd_output]
-          inv_args = return_hash[:inv_args] if return_hash[:inv_args]
-        end
-
-        $stdout.binmode.write(
-          inv_op.serialize(args: inv_args, content: fwd_output)
-        )
+      if block_given?
+        return_hash = yield(fwd_output: fwd_output, inv_args: inv_args)
+        fwd_output = return_hash[:fwd_output] if return_hash[:fwd_output]
+        inv_args = return_hash[:inv_args] if return_hash[:inv_args]
       end
+
+      $stdout.binmode.write(
+        inv_op.serialize(args: inv_args, content: fwd_output)
+      )
     end
   end
 end
