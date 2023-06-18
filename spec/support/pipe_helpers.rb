c3476d19e7ad6f8cf9a449d9a95ae027d11df6d3
Marco Imperatore <mimperatore@gmail.com>
2023-06-18 11:55:32 -0400

    calculate test coverage also for spawned ruby processes

--- spec/support/pipe_helpers.rb
+++ spec/support/pipe_helpers.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+
 module PipeHelpers
   def pexec(cmd, content)
+    SimpleCov.command_name SecureRandom.uuid
+    SimpleCov.start
+
+    cmd = "ruby -r#{__dir__}/.simplecov_spawn #{cmd}" if cmd.start_with?('bin/')
     IO.popen(cmd, 'r+') do |pipe|
       pipe.write(content)
       pipe.close_write
